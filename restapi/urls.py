8ff644c7ac23e0fe614a86586480759e7c994af3
kinz7764@naver.com <kinz7764@naver.com>
2018-10-24 20:51:02 +0900

    first commit

---
+++ restapi/urls.py
@@ -0,0 +1,26 @@
+"""kakaobot URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from . import views
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('keyboard', views.keyboard),
+    path('message', views.message),
+    path('friend', views.friend),
+    path('friend/<str:user_key>', views.friend),
+    path('chat_room/<str:user_key>', views.chat_room),
+]
---
+++ restapi/views.py
@@ -0,0 +1,97 @@
+from django.shortcuts import render
+from django.http.response import JsonResponse
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.http import Http404
+import requests
+import json
+from datetime import datetime
+
+def get_seoul_weather():
+    try:
+        r = requests.get('http://api.openweathermap.org/data/2.5/forecast/daily?lat=37.56826&lon=126.977829&APPID=cd95f9904b38f03ac8b68901cff2c720')
+        data = json.loads(r.text)
+        
+        temp = float(data["main"]["temp"]) - 273.15
+        min_temp = float(data["main"]["temp_min"]) - 273.15
+        max_temp = float(data["main"]["temp_max"]) - 273.15
+        humid = data["main"]["humidity"]
+
+        
+        msg = "오늘 서울의 온도는 {0}도, 최저 온도는 {1}도, 최고 온도는 {2)도, 습도는 {3}%입니다. ".format(temp, min_temp, max_temp, humid)
+
+        return msg
+
+    except:
+        return("날씨 api에 문제가 생겼습니다. 나중에 다시 시도해주세요!")
+
+
+def get_exchange():
+    try:
+        r = requests.get('http://earthquake.kr:23490/')
+        data = json.loads(r.text)
+
+        jtok = data['JPYKRW'][0]
+        #date_now = datetime.today().strftime("%Y/%m/%d %H:%M:%S")
+
+        ratio = 100
+        msg = "현재 환율은 {0}엔에 {1}원입니다".format(ratio, round(float(jtok) * ratio, 2))
+
+        return(msg)
+    except:
+        return("환율 api에 문제가 생겼습니다. 나중에 다시 시도해주세요!")
+
+
+def get_pretty_print(json_object):
+    dumps = json.dumps(json_object, ensure_ascii=False, sort_keys=True, indent=4, separators=(',', ': '))
+    return dumps
+
+def keyboard_default():
+    return {
+        "type" : "buttons",
+        "buttons" : ["환율", "오늘서울날씨"]
+    }
+
+def keyboard(request):
+    return \
+    HttpResponse(get_pretty_print(keyboard_default())
+               , content_type="application/json; charset=utf-8")
+
+
+@csrf_exempt
+def message(request):
+    if request.method == 'POST': 
+        data = json.loads(request.body.decode("utf-8"))
+        user_key = data["user_key"]
+        _type = data["type"]
+        content = data["content"]
+
+        msg = ""
+        print(user_key, _type, content)
+
+        if(content == "환율"):
+            msg = get_exchange()
+        elif(content == "오늘서울날씨"):
+            msg = get_seoul_weather()
+        else: 
+            msg = "잘못된 요청입니다."
+
+        message_default = {"message":{
+                "text" : msg
+            }}
+        message_default["keyboard"] = keyboard_default()
+ 
+        return \
+        HttpResponse(get_pretty_print(message_default)
+                  , content_type="application/json; charset=utf-8")
+    else:
+        raise Http404("잘못된 접근입니다.")
+
+@csrf_exempt
+def friend(request, user_key=''):
+    return HttpResponse('')
+
+@csrf_exempt
+def chat_room(request, user_key=''):
+    return HttpResponse('')
+# Create your views here.
