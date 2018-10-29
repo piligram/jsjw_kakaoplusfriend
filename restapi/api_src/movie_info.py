41b6f364f03b54721f95ff8774c93af662120859
kinz7764@naver.com <kinz7764@naver.com>
2018-10-29 18:54:22 +0900

    movie add

---
+++ restapi/api_src/movie_info.py
@@ -0,0 +1,57 @@
+import requests
+from bs4 import BeautifulSoup
+import operator
+
+naver_movie_web_url = 'https://movie.naver.com/movie/running/current.nhn'
+
+
+def get_movie_info():
+    try:
+        # 1. requests를 이용하여 크롤링하기
+        r = requests.get(naver_movie_web_url)
+
+        # Check the status code
+        if r.status_code != 200:
+            raise Exception("Can't crawl the html. {} status code".format(r.status_code))
+
+        text = r.text
+
+        # 2. beautifulsoup로 파싱
+        soup = BeautifulSoup(text, 'html.parser')
+
+        movie_list = soup.body.find(class_ = 'lst_detail_t1').find_all('li')
+
+        movie_sort = {}
+
+        movie_dict = {}
+
+
+        cnt = 0
+        for movie in movie_list:
+            cnt += 1
+            if(cnt > 15):
+                break
+            movie_title = movie.find(class_='tit').find('a').text
+            movie_score = movie.find(class_='info_star').find(class_='num').text
+            try:
+                movie_adr = movie.find(class_='info_exp').find(class_='num').text
+            except:
+                continue
+            movie_sort[movie_title] = float(movie_adr)
+            movie_dict[movie_title] = movie_score
+           
+        movie_list = sorted(movie_sort.items(), key=operator.itemgetter(1), reverse=True)
+
+        msg = ''
+
+        for movie, rate in movie_list:
+            msg += "{} : {}\n".format(movie, movie_dict[movie])
+
+        return msg
+    except Exception as e:
+        print(e)
+        return "영화 api에 문제가 생겼습니다. 나중에 다시 시도해주세요!"
+
+
+if __name__ == "__main__":
+    print(get_movie_info())
--- restapi/views.py
+++ restapi/views.py
@@ -9,43 +9,9 @@ from datetime import datetime
 
 from restapi.api_src.jypkrw_rate import get_exchange
 from restapi.api_src.weather import get_seoul_weather
+from restapi.api_src.movie_info import get_movie_info
 
-'''
-def get_seoul_weather():
-    try:
-        r = requests.get('http://api.openweathermap.org/data/2.5/forecast/daily?lat=37.56826&lon=126.977829&APPID=cd95f9904b38f03ac8b68901cff2c720')
-        data = json.loads(r.text)
-
-        temp = float(data["main"]["temp"]) - 273.15
-        min_temp = float(data["main"]["temp_min"]) - 273.15
-        max_temp = float(data["main"]["temp_max"]) - 273.15
-        humid = data["main"]["humidity"]
-
-        msg = "오늘 서울의 온도는 {}도, 최저 온도는 {}도, 최고 온도는 {}도, 습도는 {}%입니다. ".format(temp, min_temp, max_temp, humid)
-
-        return msg
-
-    except:
-        return "날씨 api에 문제가 생겼습니다. 나중에 다시 시도해주세요!"
-'''
-'''
-def get_exchange():
-    try:
-        r = requests.get('http://earthquake.kr:23490/')
-        data = json.loads(r.text)
-
-        jtok = data['JPYKRW'][0]
-        #date_now = datetime.today().strftime("%Y/%m/%d %H:%M:%S")
-
-        ratio = 100
-        msg = "현재 환율은 {}엔에 {}원입니다".format(ratio, round(float(jtok) * ratio, 2))
-
-        return(msg)
-    except:
-        return "환율 api에 문제가 생겼습니다. 나중에 다시 시도해주세요!"
-'''
-
-def get_pretty_print(json_object):
+def pretty_print(json_object):
     dumps = json.dumps(json_object, ensure_ascii=False, sort_keys=True, indent=4, separators=(',', ': '))
     return dumps
 
@@ -53,12 +19,12 @@ def get_pretty_print(json_object):
 def keyboard_default():
     return {
         "type": "buttons",
-        "buttons": ["환율", "오늘서울날씨"]
+        "buttons": ["환율", "오늘서울날씨", "영화정보"]
     }
 
 
 def keyboard(request):
-    return HttpResponse(get_pretty_print(keyboard_default()), content_type="application/json; charset=utf-8")
+    return HttpResponse(pretty_print(keyboard_default()), content_type="application/json; charset=utf-8")
 
 
 @csrf_exempt
@@ -76,6 +42,8 @@ def message(request):
             msg = get_exchange()
         elif _content == "오늘서울날씨":
             msg = get_seoul_weather()
+        elif _content == "영화정보":
+            msg = get_movie_info()
         else:
             msg = "잘못된 요청입니다."
 
@@ -84,7 +52,7 @@ def message(request):
         }}
         message_default["keyboard"] = keyboard_default()
 
-        return HttpResponse(get_pretty_print(message_default), content_type="application/json; charset=utf-8")
+        return HttpResponse(pretty_print(message_default), content_type="application/json; charset=utf-8")
     else:
         raise Http404("잘못된 접근입니다.")
 
